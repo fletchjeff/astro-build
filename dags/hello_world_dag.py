2b2050f8bbbaa2efb32f8ff83bd30fa4b4c9c5c2
fletchjeff <fletch.jeff@gmail.com>
2022-12-14 11:38:06 +0000

    Hello world DAG

---
+++ dags/hello_world_dag.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python_operator import PythonOperator
+from airflow.utils.task_group import TaskGroup
+
+args = {
+    'depends_on_past': False,
+    'retries': 0
+}
+ 
+def print_hello():
+    return 'Hello world from first Airflow DAG!'
+
+dag = DAG('hello_world_demo', description='Hello World DAG',
+          schedule_interval=None,
+          start_date=datetime(2022, 3, 20), 
+          catchup=False,
+          default_args=args
+        )
+
+with dag:        
+    hello_operator = PythonOperator(task_id='hello_task', python_callable=print_hello, dag=dag)
+
+hello_operator

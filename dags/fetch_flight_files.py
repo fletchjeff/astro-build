9221979b40f6023039282ef243ff93d25b68a9a2
Jeff Fletcher <fletch.jeff@gmail.com>
2022-12-20 12:39:28 +0100

    Update from IDE

--- dags/fetch_flight_files.py
+++ dags/fetch_flight_files.py
@@ -10,30 +10,6 @@ import pandas as pd
 import pendulum
 
 
-@aql.transform(conn_id="snowflake_jeffletcher", task_id="fetch_cancelled_flight_data")
-def fetch_cancelled_flight_data_func():
-    return """select FL_DATE, OP_CARRIER, OP_CARRIER_FL_NUM, ORIGIN, DEST, CRS_DEP_TIME, CRS_ARR_TIME, CRS_ELAPSED_TIME, DISTANCE, CANCELLED from flight_data_2 where cancelled = 1"""
-
-@aql.transform(conn_id="snowflake_jeffletcher", task_id="cancelled_flights_count")
-def cancelled_flights_count_func():
-    return """select count(*) as cancelled_flights from flight_data_2 where cancelled = 1"""
-
-@aql.dataframe(task_id="df_to_int")
-def df_to_int_func(cancelled_flights_count: pd.DataFrame):
-    return cancelled_flights_count.iloc[0]['cancelled_flights']
-
-@aql.transform(conn_id="snowflake_jeffletcher", task_id="fetch_normal_flight_data_sample")
-def fetch_normal_flight_data_sample_func(df_to_int: Table):
-    return """select FL_DATE, OP_CARRIER, OP_CARRIER_FL_NUM, ORIGIN, DEST, CRS_DEP_TIME, CRS_ARR_TIME, CRS_ELAPSED_TIME, DISTANCE, CANCELLED from flight_data_2 sample ({{df_to_int}} rows) where cancelled = 0"""
-
-@aql.dataframe(task_id="merge_rows")
-def merge_rows_func(fetch_cancelled_flight_data: pd.DataFrame, fetch_normal_flight_data_sample: pd.DataFrame):
-    print(type(fetch_cancelled_flight_data))
-    print(type(fetch_normal_flight_data_sample))
-    concat_rows = pd.concat([fetch_normal_flight_data_sample, fetch_cancelled_flight_data])
-    print(len(concat_rows))
-    return concat_rows
-
 @aql.dataframe(task_id="train_model")
 def train_model_func(merge_rows: pd.DataFrame):
     # print(merge_rows.shape)
@@ -70,8 +46,9 @@ def train_model_func(merge_rows: pd.DataFrame):
         # 'crs_arr_hour'
     ]
     
-    #mlflow.set_tracking_uri("http://astro.fletcher.za.net:5000")
-    #mlflow.set_experiment("train_model")
+    mlflow.set_tracking_uri("http://astro.fletcher.za.net:5000")
+    mlflow.set_experiment("xgboost_model")
+    mlflow.xgboost.autolog(registered_model_name="xgboost_model")
     #with mlflow.start_run(run_name=model_directory) as run:
         #mlflow.xgboost.autolog()
     
@@ -107,9 +84,39 @@ def train_model_func(merge_rows: pd.DataFrame):
     #mlflow.log_artifact(f's3://{bucketname}/models/{model_directory}/pipe.joblib')    
     
 
+@aql.transform(conn_id="snowflake_jeffletcher", task_id="fetch_cancelled_flight_data")
+def fetch_cancelled_flight_data_func():
+    return """select FL_DATE, OP_CARRIER, OP_CARRIER_FL_NUM, ORIGIN, DEST, CRS_DEP_TIME, CRS_ARR_TIME, CRS_ELAPSED_TIME, DISTANCE, CANCELLED from flight_data_2 where cancelled = 1"""
+
+@aql.dataframe(task_id="cell_1")
+def cell_1_func():
+    import os
+    import json
+    print(json.dumps(dict(os.environ),indent=4))
+
+@aql.transform(conn_id="snowflake_jeffletcher", task_id="cancelled_flights_count")
+def cancelled_flights_count_func():
+    return """select count(*) as cancelled_flights from flight_data_2 where cancelled = 1"""
+
+@aql.dataframe(task_id="df_to_int")
+def df_to_int_func(cancelled_flights_count: pd.DataFrame):
+    return cancelled_flights_count.iloc[0]['cancelled_flights']
+
+@aql.transform(conn_id="snowflake_jeffletcher", task_id="fetch_normal_flight_data_sample")
+def fetch_normal_flight_data_sample_func(df_to_int: Table):
+    return """select FL_DATE, OP_CARRIER, OP_CARRIER_FL_NUM, ORIGIN, DEST, CRS_DEP_TIME, CRS_ARR_TIME, CRS_ELAPSED_TIME, DISTANCE, CANCELLED from flight_data_2 sample ({{df_to_int}} rows) where cancelled = 0"""
+
+@aql.dataframe(task_id="merge_rows")
+def merge_rows_func(fetch_cancelled_flight_data: pd.DataFrame, fetch_normal_flight_data_sample: pd.DataFrame):
+    print(type(fetch_cancelled_flight_data))
+    print(type(fetch_normal_flight_data_sample))
+    concat_rows = pd.concat([fetch_normal_flight_data_sample, fetch_cancelled_flight_data])
+    print(len(concat_rows))
+    return concat_rows
+
 @dag(
     schedule_interval=None,
-    start_date=pendulum.from_format("2022-12-14", "YYYY-MM-DD"),
+    start_date=pendulum.from_format("2022-12-20", "YYYY-MM-DD"),
 )
 def fetch_flight_files():
     fetch_cancelled_flight_data = fetch_cancelled_flight_data_func()
@@ -131,6 +138,8 @@ def fetch_flight_files():
     train_model = train_model_func(
         merge_rows,
     )
+
+    cell_1 = cell_1_func()
 
     df_to_int << cancelled_flights_count
 
